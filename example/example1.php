65d9b9f62a6bac562230fe842d3d5087f45243f0
Ivan Habunek <ivan.habunek@gmail.com>
2012-10-17 11:16:19 +0200

    Fixed example comments.

--- example/example1.php
+++ example/example1.php
@@ -23,21 +23,21 @@ $t = Person::objects()
 
 print_r($t);
 
-// Get number of thingies born before 2012
+// Get number of people born before 2012
 $c = Person::objects()
     ->filter(f::lt('birthday', '2012-01-01'))
     ->count();
 
 echo "Count: $c\n";
 
-// Fetch all thingies whose name begins with G
+// Fetch all people whose name begins with G
 $a = Person::objects()
     ->filter(f::like('name', 'G%'))
     ->fetch();
 
 print_r($a);
 
-// Fetch all thingies with id between 54 and 57, as JSON 
+// Fetch all people with id between 54 and 57, as JSON 
 $a = Person::objects()
     ->filter(f::between('id', 54, 57))
     ->fetch(DB::FETCH_JSON);
