c3a65195df95d9f1f86b956f14868261041410e5
Ivan Habunek <ivan.habunek@gmail.com>
2012-12-02 18:25:41 +0100

    Fixed bug caused by misuse of late static binding
    Note to self: learn LSB before using it.

--- Phormium/Model.php
+++ Phormium/Model.php
@@ -18,35 +18,20 @@ abstract class Model
     protected static $_meta;
 
     /**
-     * The Meta object contructed from Model properties and data in <var>$_meta</var>.
-     * @var Meta
-     */
-    protected static $_parsedMeta;
-
-    /**
-     * The Query object used to construct an execute SQL queries for this model.
-     * @var Query
-     */
-    protected static $_query;
-
-    /**
      * Returns the Model's Meta object.
      * @return Meta
      */
     public static function getMeta()
     {
-        if (!isset(self::$_parsedMeta)) {
-            self::$_parsedMeta = self::parseMeta();
-        }
-        return self::$_parsedMeta;
+        return Parser::getMeta(
+            get_called_class(),
+            static::$_meta
+        );
     }
 
     public static function getQuery()
     {
-        if (!isset(self::$_query)) {
-            self::$_query = new Query(self::getMeta());
-        }
-        return self::$_query;
+        return new Query(self::getMeta());
     }
 
     /**
@@ -57,78 +42,6 @@ abstract class Model
         $meta = self::getMeta();
         $query = self::getQuery();
         return new QuerySet($query, $meta);
-    }
-
-    protected static function parseMeta()
-    {
-        // Late static binding at work
-        $class = get_called_class();
-        $_meta = static::$_meta;
-
-        if (!is_array($_meta)) {
-            throw new \Exception("Invalid $class::\$_meta. Not an array.");
-        }
-
-        if (empty($_meta['database'])) {
-            throw new \Exception("Invalid $class::\$_meta. Missing 'database'.");
-        }
-
-        if (empty($_meta['table'])) {
-            throw new \Exception("Invalid $class::\$_meta. Missing 'table'.");
-        }
-
-        $meta = new Meta();
-        $meta->class = $class;
-        $meta->database = $_meta['database'];
-        $meta->table = $_meta['table'];
-        $meta->pk = self::parsePK($_meta);
-        $meta->columns = array();
-
-        // Fetch class' public properties which correspond to column names
-        $rc = new \ReflectionClass($class);
-        $props = $rc->getProperties(\ReflectionProperty::IS_PUBLIC);
-        foreach ($props as $prop) {
-            $meta->columns[] = $prop->name;
-        }
-
-        if (isset($meta->pk)) {
-
-            // Verify primary key columns exist
-            foreach ($meta->pk as $column) {
-                if (!in_array($column, $meta->columns)) {
-                    throw new \Exception(
-                        "Invalid $class::\$_meta. Given primary key column [{$column}] does not exist."
-                    );
-                }
-            }
-
-            // Compile non-pk columns
-            $meta->nonPK = array();
-            foreach ($meta->columns as $column) {
-                if (!in_array($column, $meta->pk)) {
-                    $meta->nonPK[] = $column;
-                }
-            }
-        }
-
-        return $meta;
-    }
-
-    private static function parsePK($meta)
-    {
-        if (!isset($meta['pk'])) {
-            return null;
-        }
-
-        if (is_string($meta['pk'])) {
-            return array($meta['pk']);
-        }
-
-        if (is_array($meta['pk'])) {
-            return $meta['pk'];
-        }
-
-        throw new \Exception("Invalid {$meta['class']}::\$_meta['pk']. Not a string or array.");
     }
 
     /** Creates a Model instance from data in the given array. */
