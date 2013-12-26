899deb009e8bf6353585354623ea04e1a48b8eee
Ivan Habunek <ivan.habunek@gmail.com>
2013-12-26 12:40:04 +0100

    Improved stats handling, added sorted dump

--- src/Phormium/Stats.php
+++ src/Phormium/Stats.php
@@ -7,27 +7,51 @@ namespace Phormium;
  */
 class Stats
 {
-    /** Holds the stats */
+    /** Holds the stats. */
     private static $stats = array();
 
-    /** Max number of queries to keep. */
-    private static $limit = 100;
+    /** Max number of records to keep (null for no limit). */
+    private static $limit = null;
+
+    /** How many decimals to keep for timer columns. */
+    private static $precision = 4;
+
+    /** A list of columns kept for each stat record. */
+    private static $columns = array('query', 'arguments', 'prepare', 'execute', 'fetch', 'total', 'numrows');
+
+    /** A list of columns to be rounded to $precision. */
+    private static $roundColumns = array('prepare', 'execute', 'fetch', 'total');
 
     /** Add a stats array. */
     public static function add(array $stat)
     {
+        foreach(self::$roundColumns as $column) {
+            if (isset($stat[$column])) {
+                $stat[$column] = round($stat[$column], self::$precision);
+            }
+        }
+
         self::$stats[] = $stat;
 
         // Enforce max stat items
-        $count = count(self::$stats);
-        if ($count > self::$limit) {
-            self::$stats = array_slice($count - self::$limit);
+        if (isset(self::$limit) && count(self::$stats) > self::$limit) {
+            array_shift(self::$stats);
         }
     }
 
-    /** Returns all stats. */
-    public static function get()
+    /**
+     * Returns all stats.
+     *
+     * @param string $sortColumn Column to sort stats by. For possible values,
+     * see {@link $columns} If not given, stats will be ordered in order they
+     * were recorded.
+     */
+    public static function get($sortColumn = null)
     {
+        if (isset($sortColumn)) {
+            return self::getSorted($sortColumn);
+        }
+
         return self::$stats;
     }
 
@@ -37,15 +61,58 @@ class Stats
         self::$stats = array();
     }
 
-    /** Dumps statistics to stdout as a table. */
-    public static function dump()
+    /**
+     * Dumps statistics to stdout as a table.
+     *
+     * @param string $sortColumn Column to sort stats by. For possible values,
+     * see {@link $columns} If not given, stats will be ordered in order they
+     * were recorded.
+     */
+    public static function dump($sortColumn = null)
     {
-        Printer::dump(self::$stats);
+        Printer::dump(self::get($sortColumn));
     }
 
     /** Sets max number of stats records to keep. */
     public static function setLimit($limit)
     {
         self::$limit = $limit;
+    }
+
+    /** Returns collected stats, ordered by the given column. */
+    private static function getSorted($column)
+    {
+        // If prefixed by a minus, reverse sort (ascending)
+        if (isset($column[0]) && $column[0] === '-') {
+            $reverse = true;
+            $column = substr($column, 1);
+        } else {
+            $reverse = false;
+        }
+
+        if (!in_array($column, self::$columns)) {
+            trigger_error("Invalid sort column [$column].", E_USER_WARNING);
+            return self::$stats;
+        }
+
+        $stats = self::$stats;
+        $comp = self::getCompareFunction($column, $reverse);
+        usort($stats, $comp);
+        return $stats;
+    }
+
+    /** Returns a comparison function used to sort the stats collection by given column. */
+    private static function getCompareFunction($column, $reverse = false)
+    {
+        return function ($left, $right) use ($column, $reverse) {
+            if ($left[$column] == $right[$column]) {
+               return 0;
+            }
+            if ($reverse) {
+                return $left[$column] > $right[$column] ? -1 : 1;
+            } else {
+                return $left[$column] < $right[$column] ? -1 : 1;
+            }
+        };
     }
 }
