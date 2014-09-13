d9583783f09ebec8009af9094e3265961c9b1a6d
Ivan Habunek <ivan.habunek@gmail.com>
2014-09-13 14:47:16 +0200

    Fixed Travis config (for real this time)

--- tests/travis/before.sh
+++ tests/travis/before.sh
@@ -1,19 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+
+# Current foder
+SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 
 composer install
 
 mkdir tmp
 
-if [ '$DB' = 'mysql' ]; then
-    mysql < tests/travis/mysql/setup.sql
+if [ $DB = "mysql" ]
+then
+    mysql < $SCRIPT_DIR/mysql/setup.sql
 fi
 
-if [ '$DB' = 'sqlite' ]; then
-    sqlite3 tmp/test.db < tests/travis/sqlite/setup.sql
+if [ $DB = "sqlite" ]
+then
+    sqlite3 tmp/test.db < $SCRIPT_DIR/sqlite/setup.sql
 fi
 
-if [ '$DB' = 'postgres' ]; then
-    psql -c 'drop database if exists phormium_tests;' -U postgres
-    psql -c 'create database phormium_tests;' -U postgres
-    psql -f tests/travis/postgres/setup.sql -d phormium_tests -U postgres
+if [ $DB = "postgres" ]
+then
+    dropdb -U postgres --if-exists phormium_tests
+    createdb -U postgres phormium_tests
+    psql -U postgres -d phormium_tests -f $SCRIPT_DIR/postgres/setup.sql
 fi
