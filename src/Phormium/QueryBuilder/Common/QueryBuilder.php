da7294b453bd6f12198f741bc7276ccaa47e7a43
Ivan Habunek <ivan@habunek.com>
2016-12-15 09:50:48 +0100

    QueryBuilder, reorder methods, add docs

--- src/Phormium/QueryBuilder/Common/QueryBuilder.php
+++ src/Phormium/QueryBuilder/Common/QueryBuilder.php
@@ -30,142 +30,18 @@ class QueryBuilder implements QueryBuilderInterface
         $this->filterRenderer = $filterRenderer;
     }
 
-    public function renderSelect($columns, $distinct)
-    {
-        $columns = array_map($this->quoteFn, $columns);
-
-        $query = "SELECT " . ($distinct ? "DISTINCT " : "") .
-            implode(", ", $columns);
-
-        return new QuerySegment($query);
-    }
-
-    public function renderSelectAggregate(Aggregate $aggregate)
-    {
-        $column = $aggregate->column !== "*" ?
-            $this->quoter->quote($aggregate->column) : $aggregate->column;
-
-        $query = "SELECT {$aggregate->type}({$column}) AS aggregate";
-
-        return new QuerySegment($query);
-    }
-
-    public function renderFrom($table)
-    {
-        $table = $this->quoter->quote($table);
-
-        return new QuerySegment("FROM $table");
-    }
-
-    public function renderWhere(Filter $filter = null)
-    {
-        if (!isset($filter)) {
-            return new QuerySegment();
-        }
-
-        $where = new QuerySegment("WHERE");
-
-        return $where->combine($this->filterRenderer->renderFilter($filter));
-    }
-
-    /** Constructs an ORDER BY clause. */
-    public function renderOrderBy(OrderBy $orderBy = null)
-    {
-        if (!isset($orderBy)) {
-            return new QuerySegment();
-        }
-
-        $orders = array_map(function ($order) {
-            return $this->renderColumnOrder($order);
-        }, $orderBy->orders());
-
-        $query = "ORDER BY " . implode(', ', $orders);
-
-        return new QuerySegment($query);
-    }
-
-    /** Constructs a segment of the ORDER BY clause which orders by one column. */
-    public function renderColumnOrder(ColumnOrder $order)
-    {
-        $column = $this->quoter->quote($order->column());
-        $direction = strtoupper($order->direction());
-
-        return "$column $direction";
-    }
-
-    /** Constructs the LIMIT/OFFSET clause. */
-    public function renderLimitOffset(LimitOffset $limitOffset = null)
-    {
-        if (!isset($limitOffset)) {
-            return new QuerySegment();
-        }
-
-        $limit = $limitOffset->limit();
-        $offset = $limitOffset->offset();
-
-        $limitSegment = isset($limit) ?
-            new QuerySegment("LIMIT ?", [$limit]) :
-            new QuerySegment();
-
-        $offsetSegment = isset($offset) ?
-            new QuerySegment("OFFSET ?", [$offset]) :
-            new QuerySegment();
-
-        return $limitSegment->combine($offsetSegment);
-    }
-
-    /** INSERT INTO clause */
-    public function renderInsertInto($table, array $columns)
-    {
-        $table = $this->quoter->quote($table);
-        $columns = implode(", ", array_map($this->quoteFn, $columns));
-
-        return new QuerySegment("INSERT INTO $table ($columns)");
-    }
-
-    /** VALUES clause */
-    public function renderInsertValues($values)
-    {
-        $placeholders = implode(', ', array_fill(0, count($values), '?'));
-
-        return new QuerySegment("VALUES ($placeholders)", $values);
-    }
-
-    /** RETURNING clause, only used for Postgres */
-    public function renderInsertReturning($column)
-    {
-        return new QuerySegment();
-    }
-
-    public function renderUpdate($table)
-    {
-        $table = $this->quoter->quote($table);
-
-        return new QuerySegment("UPDATE $table");
-    }
-
-    public function renderUpdateSet($columns, $values)
-    {
-        $setFn = function ($column) {
-            return $this->quoter->quote($column) . " = ?";
-        };
-
-        $query = "SET " . implode(", ", array_map($setFn, $columns));
-
-        return new QuerySegment($query, $values);
-    }
-
-    protected function renderDelete($table)
-    {
-        $table = $this->quoter->quote($table);
-
-        return new QuerySegment("DELETE FROM $table");
-    }
-
-
-    // -- Build methods --------------------------------------------------------
-
-
+    /**
+     * Builds a SELECT query.
+     *
+     * @param  string           $table       Table name.
+     * @param  string[]         $columns     Array of column names.
+     * @param  Filter|null      $filter      Expression to filter by.
+     * @param  LimitOffset|null $limitOffset Fetch limit and offset.
+     * @param  OrderBy|null     $orderBy     Expression to order by.
+     * @param  boolean          $distinct    Whether to produce a SELECT DISTINCT query.
+     *
+     * @return string The SQL query.
+     */
     public function buildSelect(
         $table,
         array $columns,
@@ -216,5 +92,148 @@ class QueryBuilder implements QueryBuilderInterface
             $this->renderDelete($table),
             $this->renderWhere($filter),
         ]);
+    }
+
+
+    // -- Renderer methods -----------------------------------------------------
+
+
+    /** SELECT clause for selecting columns */
+    protected function renderSelect($columns, $distinct)
+    {
+        $columns = array_map($this->quoteFn, $columns);
+
+        $query = "SELECT " . ($distinct ? "DISTINCT " : "") .
+            implode(", ", $columns);
+
+        return new QuerySegment($query);
+    }
+
+    /** SELECT clause for selecting an aggregate */
+    protected function renderSelectAggregate(Aggregate $aggregate)
+    {
+        $column = $aggregate->column !== "*" ?
+            $this->quoter->quote($aggregate->column) : $aggregate->column;
+
+        $query = "SELECT {$aggregate->type}({$column}) AS aggregate";
+
+        return new QuerySegment($query);
+    }
+
+    /** FROM clause */
+    protected function renderFrom($table)
+    {
+        $table = $this->quoter->quote($table);
+
+        return new QuerySegment("FROM $table");
+    }
+
+    /** WHERE clause */
+    protected function renderWhere(Filter $filter = null)
+    {
+        if (!isset($filter)) {
+            return new QuerySegment();
+        }
+
+        $where = new QuerySegment("WHERE");
+
+        return $where->combine($this->filterRenderer->renderFilter($filter));
+    }
+
+    /** ORDER BY clause */
+    protected function renderOrderBy(OrderBy $orderBy = null)
+    {
+        if (!isset($orderBy)) {
+            return new QuerySegment();
+        }
+
+        $orders = array_map(function ($order) {
+            return $this->renderColumnOrder($order);
+        }, $orderBy->orders());
+
+        $query = "ORDER BY " . implode(', ', $orders);
+
+        return new QuerySegment($query);
+    }
+
+    /** ORDER BY clause fragment ordering by one column */
+    protected function renderColumnOrder(ColumnOrder $order)
+    {
+        $column = $this->quoter->quote($order->column());
+        $direction = strtoupper($order->direction());
+
+        return "$column $direction";
+    }
+
+    /** LIMIT and OFFSET clauses */
+    protected function renderLimitOffset(LimitOffset $limitOffset = null)
+    {
+        if (!isset($limitOffset)) {
+            return new QuerySegment();
+        }
+
+        $limit = $limitOffset->limit();
+        $offset = $limitOffset->offset();
+
+        $limitSegment = isset($limit) ?
+            new QuerySegment("LIMIT ?", [$limit]) :
+            new QuerySegment();
+
+        $offsetSegment = isset($offset) ?
+            new QuerySegment("OFFSET ?", [$offset]) :
+            new QuerySegment();
+
+        return $limitSegment->combine($offsetSegment);
+    }
+
+    /** INSERT INTO clause */
+    protected function renderInsertInto($table, array $columns)
+    {
+        $table = $this->quoter->quote($table);
+        $columns = implode(", ", array_map($this->quoteFn, $columns));
+
+        return new QuerySegment("INSERT INTO $table ($columns)");
+    }
+
+    /** VALUES clause for use with INSERT */
+    protected function renderInsertValues($values)
+    {
+        $placeholders = implode(', ', array_fill(0, count($values), '?'));
+
+        return new QuerySegment("VALUES ($placeholders)", $values);
+    }
+
+    /** RETURNING clause, only used for Postgres */
+    protected function renderInsertReturning($column)
+    {
+        return new QuerySegment();
+    }
+
+    /** UPDATE clause */
+    protected function renderUpdate($table)
+    {
+        $table = $this->quoter->quote($table);
+
+        return new QuerySegment("UPDATE $table");
+    }
+
+    /* SET clause, for use with UPDATE */
+    protected function renderUpdateSet($columns, $values)
+    {
+        $setFn = function ($column) {
+            return $this->quoter->quote($column) . " = ?";
+        };
+
+        $query = "SET " . implode(", ", array_map($setFn, $columns));
+
+        return new QuerySegment($query, $values);
+    }
+
+    /** DELETE clause */
+    protected function renderDelete($table)
+    {
+        $table = $this->quoter->quote($table);
+
+        return new QuerySegment("DELETE FROM $table");
     }
 }
