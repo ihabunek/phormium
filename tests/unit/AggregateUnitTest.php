52c6bcc26d147370c569703d5194f7f4e89a4084
Ivan Habunek <ivan.habunek@gmail.com>
2016-12-23 16:21:22 +0100

    Start separating unit tests from integration tests

---
+++ tests/unit/AggregateUnitTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Phormium\Tests\Unit;
+
+use Phormium\Query\Aggregate;
+
+/**
+ * @group aggregate
+ * @group unit
+ */
+class AggregateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $agg = new Aggregate(Aggregate::AVERAGE, "foo");
+        $this->assertSame("avg", $agg->type());
+        $this->assertSame("foo", $agg->column());
+
+        $agg = new Aggregate(Aggregate::COUNT);
+        $this->assertSame("count", $agg->type());
+        $this->assertSame("*", $agg->column());
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Invalid aggregate type [xxx].
+     */
+    public function testInvalidType()
+    {
+        $agg = new Aggregate('xxx', 'yyy');
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Aggregate type [avg] requires a column to be given.
+     */
+    public function testRequiresColumnError()
+    {
+        $agg = new Aggregate(Aggregate::AVERAGE);
+    }
+}
